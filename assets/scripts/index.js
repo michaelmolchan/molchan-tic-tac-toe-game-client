2a67ceae9a2d3dea625868da962be5c1b6b1ced5
michaelmolchan <mmolchan91@gmail.com>
2018-06-11 15:33:10 -0400

    Add click handler for each individual cell
    - When a cell is clicked on it will display an 'X' or an 'O' depending
    upon which player's turn it is.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,9 +3,54 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events')
+// const themeStyles = require('../styles/theme.scss')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+
+// Keeps track of the score
+let score = {
+  'X': 0,
+  'O': 0
+}
+
+// Assigns 'X' and 'O' to players
+const playerOne = 'X'
+
+const playerTwo = 'O'
+
+// Represents whose turn it is
+let turn = 'X'
+
+// Represents an empty Gameboard
+const gameboard = [
+  0, 0, 0,
+  0, 0, 0,
+  0, 0, 0
+]
+
+// const winCombinations = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6]
+// ]
+
+const onClickBoard = function (event) {
+  console.log('onClickBoard ran', event.target.id)
+  const boxId = event.target.id
+  console.log(boxId)
+  if (turn === 'X') {
+    $(this).toggleClass('square-x')
+  } else {
+    $(this).toggleClass('square-o')
+  }
+  // gameboard[0] = 'X'
+}
 
 $(() => {
   // your JS code goes here
@@ -14,4 +59,13 @@ $(() => {
   $('#sign-in-form').on('submit', authEvents.onSignIn)
   $('#change-password-form').on('submit', authEvents.onChangePassword)
   $('#sign-out').on('click', authEvents.onSignOut)
+  $('#box-0').on('click', onClickBoard)
+  $('#box-1').on('click', onClickBoard)
+  $('#box-2').on('click', onClickBoard)
+  $('#box-3').on('click', onClickBoard)
+  $('#box-4').on('click', onClickBoard)
+  $('#box-5').on('click', onClickBoard)
+  $('#box-6').on('click', onClickBoard)
+  $('#box-7').on('click', onClickBoard)
+  $('#box-8').on('click', onClickBoard)
 })
