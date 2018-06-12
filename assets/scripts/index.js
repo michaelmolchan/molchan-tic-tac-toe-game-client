5ef390c6d8c434026a3c41db187c09204145e52e
michaelmolchan <mmolchan91@gmail.com>
2018-06-12 16:32:18 -0400

    Add code that stops game when winner is identified
    - HTML displays message when a player wins
    - HTML displays message when user clicks board after a player has won

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,10 +9,10 @@ const authEvents = require('./auth/events')
 // require('./example')
 
 // Keeps track of the score
-let score = {
-  'X': 0,
-  'O': 0
-}
+// let score = {
+//   'X': 0,
+//   'O': 0
+// }
 
 // Assigns 'X' and 'O' to players
 const playerOne = 'X'
@@ -51,55 +51,81 @@ const winCombos = function () {
     [gameboard[2], gameboard[4], gameboard[6]]
   ]
 }
+
+let gameOver = false
+
 // Checks the gameboard for a win after each turn
 const checkForWin = function () {
   let i
   for (i = 0; i < winCombos().length; i++) {
     if (winCombos()[i].join() === 'X,X,X') {
       console.log('Player One Wins!')
+      winnerX()
     } else if (winCombos()[i].join() === 'O,O,O') {
       console.log('Player Two Wins!')
+      winnerO()
     } else {}
   }
+}
+
+const winnerX = function () {
+  // Add html to div that says 'PLAYER ONE WINS'
+  $('.winner-alert').append('PLAYER ONE WINS!')
+  // Indicates that the game is over
+  gameOver = true
+  // Add 1 to playerOne's score
+}
+
+const winnerO = function () {
+  // Add html to div that says 'PLAYER ONE WINS'
+  $('.winner-alert').append('PLAYER TWO WINS!')
+  // Indicates that the game is over
+  gameOver = true
+  // Add 1 to playerOne's score
 }
 
 const onClickBoard = function (event) {
   event.preventDefault()
   console.log('You clicked on ', event.target.id)
-  if (playerTurn === 'X') {
-    // check to see if the cell has empty cell class (.square), if so replace with class '.square-x'
-    if ($(this).hasClass('square')) {
-      $(this).removeClass('square')
-      $(this).addClass('square-x')
-      // Add 'X' to the gameboard array index that represents the cell that was clicked
-      gameboard[event.target.id] = playerTurn
-      console.log(gameboard)
-      // Check the updated gameboard for win combos
-      checkForWin()
-      playerTurn = 'O'
-    } else {
-      console.log('This cell has been selected')
+  // if a winner has not been identified, run this code
+  if (gameOver === false) {
+    if (playerTurn === 'X') {
+      // check to see if the cell has empty cell class (.square), if so replace with class '.square-x'
+      if ($(this).hasClass('square')) {
+        $(this).removeClass('square')
+        $(this).addClass('square-x')
+        // Adds 'X' to the gameboard array index that represents the cell that was clicked
+        gameboard[event.target.id] = playerTurn
+        console.log(gameboard)
+        // Checks the updated gameboard for win combos
+        checkForWin()
+        playerTurn = 'O'
+      } else {
+        console.log('This cell has been selected')
+      }
+      console.log(playerTurn)
+    } else if (playerTurn === 'O') {
+      // checks to see if the cell has empty cell class (.square), if so replace with class '.square-o'
+      if ($(this).hasClass('square')) {
+        $(this).removeClass('square')
+        $(this).addClass('square-o')
+        // Adds 'O' to the gameboard array index that represents the cell that was clicked
+        gameboard[event.target.id] = playerTurn
+        console.log(gameboard)
+        // Checks the updated gameboard for win combos
+        checkForWin()
+        playerTurn = 'X'
+      } else {
+        console.log('This cell has been selected')
+      }
+      console.log(playerTurn)
     }
-    console.log(playerTurn)
-  } else if (playerTurn === 'O') {
-    // check to see if the cell has empty cell class (.square), if so replace with class '.square-o'
-    if ($(this).hasClass('square')) {
-      $(this).removeClass('square')
-      $(this).addClass('square-o')
-      // Add 'O' to the gameboard array index that represents the cell that was clicked
-      gameboard[event.target.id] = playerTurn
-      console.log(gameboard)
-      // Check the updated gameboard for win combos
-      checkForWin()
-      playerTurn = 'X'
-    } else {
-      console.log('This cell has been selected')
-    }
-    console.log(playerTurn)
+  } else {
+    $('.game-over-alert').html('Click Reset Board to start a new game!')
   }
 }
 
-// Resets the gameboard
+// Resets the gameboard, set player turn to 'X'
 const resetBoard = function (event) {
   event.preventDefault()
   $('.square-x').addClass('square')
@@ -110,13 +136,13 @@ const resetBoard = function (event) {
 }
 
 // Clears the scoreboard
-const clearScore = function () {
-  score = {
-    'X': 0,
-    'O': 0
-  }
-  console.log(score)
-}
+// const clearScore = function () {
+//   score = {
+//     'X': 0,
+//     'O': 0
+//   }
+//   console.log(score)
+// }
 
 $(() => {
   // your JS code goes here
@@ -135,5 +161,5 @@ $(() => {
   $('#7').on('click', onClickBoard)
   $('#8').on('click', onClickBoard)
   $('#reset-board').on('click', resetBoard)
-  $('#clear-score').on('click', clearScore)
+  // $('#clear-score').on('click', clearScore)
 })
